194953464b85cfa5115bc25c73a691ee63f70309
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2025-04-22 23:30:53 +1200

    style: adjust layout and spacing in home page components
    Refactor the layout to center content and adjust padding for better visual consistency. Update container widths and spacing to improve responsiveness and alignment.

--- app/page.tsx
+++ app/page.tsx
@@ -31,8 +31,8 @@ export default function Home() {
         >
           <IdBadge></IdBadge>
 
-          <div className="flex items-center pr-[20rem] py-20 md:py-32 h-full">
-            <div className="flex container px-4 md:px-14 mx-auto max-w-4xl items-center justify-between">
+          <div className="flex items-center justify-center py-20 md:py-32 h-full">
+            <div className="flex container px-4 md:px-14 !pr-[20rem] mx-auto max-w-5xl items-center justify-between">
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2 z-20">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none z-20">
@@ -97,7 +97,7 @@ export default function Home() {
 
         {/* Current Work Section */}
         <section id="current" className="py-12 md:py-24 bg-muted/50">
-          <div className="container px-4 md:px-6">
+          <div className="container px-4 md:px-6 mx-auto max-w-4xl ">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
@@ -109,7 +109,7 @@ export default function Home() {
                 </p>
               </div>
             </div>
-            <div className="mx-auto grid max-w-4xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
+            <div className="mx-auto grid items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
               <Image
                 src="/placeholder.svg?height=400&width=600"
                 width={600}
